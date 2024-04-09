96102785cf12849d1bd5531df223c0f798512ec7
Rosa Rivera <riverasoto.rosa@gmail.com>
2024-04-09 10:48:04 -0400

    Create assignment1.asm

---
+++ assignment1.asm
@@ -0,0 +1,10 @@
+ADD $s1,$s6,$s6
+ADD $s2,$s1,$s1
+ADD $s3,$s2,$s2
+ADD $s4,$s3,$s3
+ADD $s5,$s4,$s4
+ADD $s7,$s5,$s1
+
+#The value of the int val is stored in the register $s6. Write a piece of MIPS Q : code that computes and places in the register $s7 the value of 30*val.
+#The following conditions should be met:
+#the whole answer should be no longer than 10 instructions. only ADD, ADDI, and SUB instructions can be used. Hint: Please think about the following fact “4x + 4x = 8x”.
